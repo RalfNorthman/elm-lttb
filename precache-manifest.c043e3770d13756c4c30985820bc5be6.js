128b839ef2db919a65312ce14b67a56eae5bfdfb
Ralf Northman <ralf.northman@bredband.net>
2020-09-14 08:40:14 +0200

    Updates

---
+++ precache-manifest.c043e3770d13756c4c30985820bc5be6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-lttb/favicon.ico"
+  },
+  {
+    "revision": "fdbcdeb058609454b9b7c00793efa9b0",
+    "url": "/elm-lttb/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-lttb/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-lttb/manifest.json"
+  },
+  {
+    "revision": "0e8b1e26091255e6fcf5",
+    "url": "/elm-lttb/static/css/main.7f457dfb.chunk.css"
+  },
+  {
+    "revision": "0e8b1e26091255e6fcf5",
+    "url": "/elm-lttb/static/js/main.0e8b1e26.chunk.js"
+  },
+  {
+    "revision": "b49bd93859e9d1ff53e3",
+    "url": "/elm-lttb/static/js/runtime~main.b49bd938.js"
+  },
+  {
+    "revision": "b9063112a1dd4b93007d",
+    "url": "/elm-lttb/static/js/vendors~main.b9063112.chunk.js"
+  }
+]);
